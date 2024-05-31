faf70f8cf32180a842035f1e2eb74c36dbbbd2ae
ja-errorpro <compileerr0r@ja-errorpro.codes>
2024-05-31 17:03:23 +0800

    add buffer handling

--- DS2ex5_17_11127137_11127150/DS2ex5_17_11127137_11127150.cpp
+++ DS2ex5_17_11127137_11127150/DS2ex5_17_11127137_11127150.cpp
@@ -58,7 +58,9 @@ class FileHandler {
     int split_count;
 
    public:
-    FileHandler() : split_count(0) {}
+    int64_t internal_sort_duration;
+    int64_t external_sort_duration;
+    FileHandler() : split_count(0), internal_sort_duration(0), external_sort_duration(0) {}
     ~FileHandler() { clear(); }
     void clear() {
         input_file_name.clear();
@@ -109,9 +111,34 @@ class FileHandler {
         if (file_size % (MAX_BUFFER_SIZE * sizeof(StudentData)) != 0) split_count++;
     }
 
+    void floatRadixSort(vector<StudentData> &data) {
+        for (int i = 0; i < data.size(); ++i) {
+            reinterpret_cast<int &>(data[i].weight) = reinterpret_cast<int &>(data[i].weight) | 0x80000000;
+        }
+        vector<StudentData> bucket[256];
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < data.size(); ++j) {
+                int index = (reinterpret_cast<int &>(data[j].weight) >> (i * 8)) & 0xff;
+                bucket[index].push_back(data[j]);
+            }
+            int index = 0;
+            for (int j = 0; j < 256; ++j) {
+                for (int k = 0; k < bucket[j].size(); ++k) {
+                    data[index++] = bucket[j][k];
+                }
+                bucket[j].clear();
+            }
+        }
+        for (int i = 0; i < data.size(); ++i) {
+            reinterpret_cast<int &>(data[i].weight) = reinterpret_cast<int &>(data[i].weight) & 0x7fffffff;
+        }
+        reverse(data.begin(), data.end());
+    }
+
     void splitData() {
         ifstream fin(input_file_name, ios::binary);
         calculateSplitCount();
+        internal_sort_duration = 0;
         for (int i = 0; i < split_count; i++) {
             partial_student_data.clear();
             for (int j = 0; j < MAX_BUFFER_SIZE; j++) {
@@ -120,9 +147,14 @@ class FileHandler {
                 if (fin.eof()) break;
                 partial_student_data.push_back(student);
             }
+            auto start = chrono::high_resolution_clock::now();
             stable_sort(
                 partial_student_data.begin(), partial_student_data.end(),
                 [](const StudentData &a, const StudentData &b) { return a.weight - b.weight > 1e-6; });
+            // floatRadixSort(partial_student_data);
+            auto end = chrono::high_resolution_clock::now();
+            int64_t duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
+            internal_sort_duration += duration;
             writePartialSortedData(i);
         }
         fin.close();
@@ -157,6 +189,49 @@ class FileHandler {
         fout.close();
     }
 
+    void twoWayMergeFileBuffer(string left_file, string right_file, string tmp_file, int buffer_size) {
+        ifstream left_fin("tmp_" + left_file, ios::binary);
+        ifstream right_fin("tmp_" + right_file, ios::binary);
+        ofstream fout("tmp_" + tmp_file, ios::binary);
+        StudentData *left_student = new StudentData[buffer_size];
+        StudentData *right_student = new StudentData[buffer_size];
+        StudentData *out_student = new StudentData[buffer_size * 2];
+        left_fin.read((char *)left_student, buffer_size * sizeof(StudentData));
+        right_fin.read((char *)right_student, buffer_size * sizeof(StudentData));
+        int l = 0, r = 0;
+        int out = 0;
+        while (l < buffer_size && r < buffer_size) {
+            if (left_student[l].weight >= right_student[r].weight) {
+                out_student[out] = left_student[l];
+                l++;
+
+            } else {
+                out_student[out] = right_student[r];
+                r++;
+            }
+            out++;
+        }
+        while (l < buffer_size) {
+            out_student[out] = left_student[l];
+            l++;
+            out++;
+        }
+        while (r < buffer_size) {
+            out_student[out] = right_student[r];
+            r++;
+            out++;
+        }
+        if (out > 0) {
+            fout.write((char *)out_student, out * sizeof(StudentData));
+        }
+        delete[] left_student;
+        delete[] right_student;
+        delete[] out_student;
+        left_fin.close();
+        right_fin.close();
+        fout.close();
+    }
+
     void mergeFile() {
         vector<string> file_name;
         set<string> all_file;
@@ -164,7 +239,9 @@ class FileHandler {
             file_name.push_back(to_string(i));
             all_file.insert(to_string(i));
         }
+        external_sort_duration = 0;
         cout << "\nNow there are " << file_name.size() << " runs." << endl;
+        int level = 1;
         while (file_name.size() > 1) {
             vector<string> new_file_name;
             for (int i = 0; i < file_name.size(); i += 2) {
@@ -172,7 +249,13 @@ class FileHandler {
                     string l = file_name[i];
                     string r = file_name[i + 1];
                     string t = "merging";
-                    twoWayMergeFile(l, r, t);
+                    int buffer_size = level * MAX_BUFFER_SIZE;
+                    auto start = chrono::high_resolution_clock::now();
+                    // twoWayMergeFile(l, r, t);
+                    twoWayMergeFileBuffer(l, r, t, buffer_size);
+                    auto end = chrono::high_resolution_clock::now();
+                    int64_t duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
+                    external_sort_duration += duration;
                     remove(("tmp_" + l).c_str());
                     rename(("tmp_" + t).c_str(), ("tmp_" + l).c_str());
                     remove(("tmp_" + t).c_str());
@@ -184,11 +267,13 @@ class FileHandler {
                     all_file.insert(file_name[i]);
                 }
             }
+            level++;
             file_name = new_file_name;
             cout << "\nNow there are " << file_name.size() << " runs." << endl;
         }
-        /*rename(("tmp_" + file_number + "_" + file_name[0]).c_str(),
-               ("sorted" + file_number + ".bin").c_str());*/
+        rename(("tmp_" + file_number + "_" + file_name[0]).c_str(),
+               ("sorted" + file_number + ".bin").c_str());
+        /*
         ifstream fin("tmp_" + file_name[0], ios::binary);
         // cerr << file_name[0] << endl;
         // assert(fin.is_open());
@@ -198,7 +283,7 @@ class FileHandler {
             fout.write((char *)&student, sizeof(StudentData));
         }
         fin.close();
-        fout.close();
+        fout.close();*/
         for (auto &file : all_file) {
             remove(("tmp_" + file).c_str());
         }
@@ -245,23 +330,26 @@ class Solution {
         WriteCase1Menu();
         int read_result = fileHandler.read();
         while (read_result == -1) {
+            fileHandler.clear();
             read_result = fileHandler.read();
         }
         if (read_result == -2) return;
-        auto start = chrono::high_resolution_clock::now();
+        // auto start = chrono::high_resolution_clock::now();
         fileHandler.splitData();
         cout << "\nThe internal sort is completed. Check the initial sorted runs!" << endl;
-        auto end = chrono::high_resolution_clock::now();
-        int64_t split_duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
-        start = chrono::high_resolution_clock::now();
+        // auto end = chrono::high_resolution_clock::now();
+        // int64_t split_duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
+        // start = chrono::high_resolution_clock::now();
         fileHandler.mergeFile();
-        end = chrono::high_resolution_clock::now();
-        int64_t merge_duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
+        // end = chrono::high_resolution_clock::now();
+        // int64_t merge_duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
         cout << "\nThe execution time ...";
-        cout << "\nInternal Sort = " << fixed << setprecision(3) << (float)split_duration / 1000 << " ms";
-        cout << "\nExternal Sort = " << (float)merge_duration / 1000 << " ms";
-        cout << "\nTotal Execution Time = " << (float)(split_duration + merge_duration) / 1000 << " ms"
-             << endl;
+        cout << "\nInternal Sort = " << fixed << setprecision(3)
+             << (float)fileHandler.internal_sort_duration / 1000 << " ms";
+        cout << "\nExternal Sort = " << (float)fileHandler.external_sort_duration / 1000 << " ms";
+        cout << "\nTotal Execution Time = "
+             << (float)(fileHandler.internal_sort_duration + fileHandler.external_sort_duration) / 1000
+             << " ms" << endl;
         cout.unsetf(ios::fixed);
     }
 
